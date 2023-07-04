debe8167ba0058873553149fca932b803be5b633
dschaele <daniel.schaele@gmx.de>
2023-07-04 06:08:22 -0700

    add teach to base plate example

--- examples/plot_base_plate.py
+++ examples/plot_base_plate.py
@@ -9,6 +9,12 @@ my_conf1_robot = cg.get_robot_config_1(link1=0.3, link1_offset=0.0,
                                        link3=0.3, link3_offset=0.0,
                                        link4=0.2, link4_offset=0.0)
 
+# Plot the robot on base plate
 robot_plot = my_conf1_robot.plot(my_conf1_robot.q, backend='pyplot')
 pt.plot_baseplate(robot_plot)
-robot_plot.hold()
+# robot_plot.hold()
+
+# Jog the robot on base plate
+robot_teach = my_conf1_robot.teach(my_conf1_robot.q, backend='pyplot', block=False)
+pt.plot_baseplate(robot_teach)
+robot_teach.hold()
